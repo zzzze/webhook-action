3bf10599f8c889362bd3934baa1d6014f2f098ad
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-09-26 15:47:16 +0800

    modify for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-echo "$1"
+echo "'$1'"
 echo "$2"
 echo "$3"
-curl -X POST $2 -H 'Accept: application/vnd.github.everest-preview+json' -H 'Authorization: token $TOKEN' --data "'$1'"
+curl -X POST $2 -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data "'$1'"
 # curl -X POST $2 -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data "'$1'"
 # curl -X POST $2 --data "'$1'" $3
