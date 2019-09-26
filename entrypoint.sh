27aa5f39bb75618bd0f7fa4c8194eda6197212c7
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-09-26 14:19:27 +0800

    modify entrypoint.sh for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eu
-echo $WEBHOOK_URL
-echo "Authorization: token $WEBHOOK_URL"
+echo $2
+echo "Authorization: token $2"
 curl -X POST $2 -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data $1
