1c1a1f99a46e5c6259fe951a10e2911dc1a71ebd
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-09-26 14:14:44 +0800

    add action.yml

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 set -eu
 echo $WEBHOOK_URL
 echo "Authorization: token $WEBHOOK_URL"
-curl -X POST $WEBHOOK_URL -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data "{\"event_type\": \"CUSTOM_ACTION_NAME_HERE\"}"
+curl -X POST $2 -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data $1
