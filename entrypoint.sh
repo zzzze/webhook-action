a8942db663d963f3e6c18fb3bbb96c38793325fb
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-09-26 19:01:53 +0800

    modify for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,8 @@ echo "'$1'"
 echo "$2"
 echo "$3"
 curl -X POST --data "$1" $3 $2
-str="curl -X POST \$3 --data \"\$1\" $2"
+command="curl -X POST \$3 --data \"\$1\" $2"
+eval $command
+
 # curl -X POST $2 -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data "'$1'"
 # curl -X POST $2 --data "'$1'" $3
