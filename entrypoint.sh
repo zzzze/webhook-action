eeb0cfa88d1d462d9c4e63008c9cf57b3c3de712
zzzze <32935001+zzzze@users.noreply.github.com>
2019-09-26 11:27:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -eu
-echo $curl
-$curl
+echo $WEBHOOK_URL
+echo "Authorization: token $WEBHOOK_URL"
+curl -X POST $WEBHOOK_URL -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data '{"event_type": "CUSTOM_ACTION_NAME_HERE"}
