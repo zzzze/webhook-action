d7658dc5259d4d11b9f16b83e0c98f2d0a4ee069
zhangzeshuan <zhangzeshuan@ifanr.com>
2019-09-27 09:57:17 +0800

    use 'eval' to run 'curl' command

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,3 @@
 #!/bin/sh
-echo "'$1'"
-echo "$2"
-echo "$3"
 command="curl -X POST $3 --data \"\$1\" $2"
-echo $command
 eval $command
-
-# curl -X POST $2 -H "Accept: application/vnd.github.everest-preview+json" -H "Authorization: token $TOKEN" --data "'$1'"
-# curl -X POST $2 --data "'$1'" $3
